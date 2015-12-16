6b0d462e4f8110baa99080c4a8b83dafc6c0c3b3
Dolphin Wood <dolphin.w.e@gmail.com>
2015-12-17 01:09:26 +0800

    delete(options): delete useless option

--- src/smooth_scrollbar.js
+++ src/smooth_scrollbar.js
@@ -9,7 +9,6 @@ import { sbList } from './shared/sb_list';
 
 const DEFAULT_OPTIONS = {
     SPEED: 1, // scroll speed scale
-    DRAG_EDGE: 50, // container's drag edge offset (px)
     STEP_LENGTH: 50, // wheel scroll step length (px/delta)
     EASING_DURATION: 1e3, // swipe easing duration (ms)
     EASING_CURVE: 'cubic-bezier(0.1, 0.57, 0.1, 1)' // cubic bezier easing function
