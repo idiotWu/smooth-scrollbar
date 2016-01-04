1cf85deada3286e7d4f6da5d4ad223dc28108942
Dolphin Wood <dolphin.w.e@gmail.com>
2016-01-04 18:03:21 +0800

    fix(addEvent): fix event propagation

--- src/internals/add_event.js
+++ src/internals/add_event.js
@@ -8,21 +8,24 @@ import { SmoothScrollbar } from '../smooth_scrollbar';
 
 export { SmoothScrollbar };
 
-function __addEvent(elem, events, handler) {
+function __addEvent(elem, events, fn) {
     if (!elem || typeof elem.addEventListener !== 'function') {
         throw new TypeError(`expect elem to be a DOM element, but got ${elem}`);
+    }
+
+    let handler = fn;
+
+    if (!this.eventPropagation) {
+        handler = (e, ...args) => {
+            e.stopPropagation();
+            fn(e, ...args);
+        };
     }
 
     events.split(/\s+/g).forEach((evt) => {
         this.__handlers.push({ evt, elem, handler });
 
-        elem.addEventListener(evt, (e, ...args) => {
-            if (!this.eventPropagation) {
-                e.stopPropagation();
-            }
-
-            handler(e, ...args);
-        });
+        elem.addEventListener(evt, handler);
     });
 };
 
