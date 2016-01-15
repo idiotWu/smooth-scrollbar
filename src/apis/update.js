cdaae30dfa9b7671b761b2d5dbe4de492c42b374
Dolphin Wood <dolphin.w.e@gmail.com>
2016-01-15 14:45:08 +0800

    refact: update scrollbar synchronized

--- src/apis/update.js
+++ src/apis/update.js
@@ -14,49 +14,43 @@ export { SmoothScrollbar };
 /**
  * @method
  * @api
- * Update scrollbars appearance in next animation frame
- *
- * @param {Function} cb: callback
+ * Update scrollbars appearance
  */
-SmoothScrollbar.prototype.update = function(cb) {
-    requestAnimationFrame(() => {
-        this.__updateBounding();
+SmoothScrollbar.prototype.update = function() {
+    this.__updateBounding();
 
-        let size = this.getSize();
+    let size = this.getSize();
 
-        this.__readonly('size', size);
+    this.__readonly('size', size);
 
-        let newLimit = {
-            x: size.content.width - size.container.width,
-            y: size.content.height - size.container.height
-        };
+    let newLimit = {
+        x: size.content.width - size.container.width,
+        y: size.content.height - size.container.height
+    };
 
-        if (this.limit &&
-            newLimit.x === this.limit.x &&
-            newLimit.y === this.limit.y) return;
+    if (this.limit &&
+        newLimit.x === this.limit.x &&
+        newLimit.y === this.limit.y) return;
 
-        this.__readonly('limit', newLimit);
+    this.__readonly('limit', newLimit);
 
-        let { xAxis, yAxis } = this.targets;
+    let { xAxis, yAxis } = this.targets;
 
-        // hide scrollbar if content size less than container
-        setStyle(xAxis.track, {
-            'display': size.content.width <= size.container.width ? 'none' : 'block'
-        });
-        setStyle(yAxis.track, {
-            'display': size.content.height <= size.container.height ? 'none' : 'block'
-        });
-
-        // use percentage value for thumb
-        setStyle(xAxis.thumb, {
-            'width': `${pickInRange(size.container.width / size.content.width, 0, 1) * 100}%`
-        });
-        setStyle(yAxis.thumb, {
-            'height': `${pickInRange(size.container.height / size.content.height, 0, 1) * 100}%`
-        });
-
-        this.__setThumbPosition();
-
-        if (typeof cb === 'function') cb(this);
+    // hide scrollbar if content size less than container
+    setStyle(xAxis.track, {
+        'display': size.content.width <= size.container.width ? 'none' : 'block'
     });
+    setStyle(yAxis.track, {
+        'display': size.content.height <= size.container.height ? 'none' : 'block'
+    });
+
+    // use percentage value for thumb
+    setStyle(xAxis.thumb, {
+        'width': `${pickInRange(size.container.width / size.content.width, 0, 1) * 100}%`
+    });
+    setStyle(yAxis.thumb, {
+        'height': `${pickInRange(size.container.height / size.content.height, 0, 1) * 100}%`
+    });
+
+    this.__setThumbPosition();
 };
