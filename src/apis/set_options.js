9aaa1cfbfb9d26d69ad7f4de60bf896cd13aad8c
Dolphin Wood <dolphin.w.e@gmail.com>
2016-02-25 08:10:01 +0800

    fix(setOption): filter empty props

--- src/apis/set_options.js
+++ src/apis/set_options.js
@@ -18,6 +18,11 @@ export { SmoothScrollbar };
  */
 SmoothScrollbar.prototype.setOptions = function(options = {}) {
     Object.keys(options).forEach((prop) => {
+        if (isNaN(parseFloat(options[prop]))) {
+            delete options[prop];
+            return;
+        }
+
         if (!OPTION_LIMIT.hasOwnProperty(prop)) return;
 
         options[prop] = pickInRange(options[prop], ...OPTION_LIMIT[prop]);
