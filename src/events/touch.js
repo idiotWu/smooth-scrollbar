07e6bc2a91eaa7141827f32acd05112291153fbc
Dolphin Wood <dolphin.w.e@gmail.com>
2016-03-08 21:23:07 +0800

    remove(touch): remove touch precision detect

--- src/events/touch.js
+++ src/events/touch.js
@@ -77,6 +77,8 @@ let __touchHandler = function() {
 
         if (!lastTouchPos) return;
 
+        evt.preventDefault();
+
         let duration = Date.now() - lastTouchTime;
         let { x: lastX, y: lastY } = lastTouchPos;
         let { x: curX, y: curY } = lastTouchPos = getPosition(evt);
@@ -88,12 +90,6 @@ let __touchHandler = function() {
 
         let destX = pickInRange(lastX - curX + offset.x, 0, limit.x);
         let destY = pickInRange(lastY - curY + offset.y, 0, limit.y);
-
-        if (Math.abs(destX - offset.x) < 1 && Math.abs(destY - offset.y) < 1) {
-            return this.__updateThrottle();
-        }
-
-        evt.preventDefault();
 
         this.setPosition(destX, destY);
     });
