09bcb8166110b189f06ddb29dfa2c102a7b15772
Dolphin Wood <dolphin.w.e@gmail.com>
2016-04-12 23:00:54 +0900

    feat: clearMovement method

--- src/apis/index.js
+++ src/apis/index.js
@@ -6,5 +6,6 @@ export * from './scroll_to';
 export * from './set_options';
 export * from './set_position';
 export * from './toggle_track';
+export * from './clear_movement';
 export * from './infinite_scroll';
 export * from './get_content_elem';
