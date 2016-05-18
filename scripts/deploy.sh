0374f0a9fb145e4d36aa76664c614b9370812436
Dolphin Wood <dolphin.w.e@gmail.com>
2016-05-19 00:13:09 +0900

    fix travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,6 @@ TARGET_BRANCH="gh-pages"
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
-    doCompile
     exit 0
 fi
 
@@ -20,18 +19,19 @@ SHA=`git rev-parse --verify HEAD`
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
 git clone $REPO out
 cd out
-git checkout $TARGET_BRANCH
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 
+# Clean out existing contents
+cd ..
+rm -rf ./out/**/* || exit 0
+cd out
 
 # Now let's go have some fun with the cloned repo
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
+# Copy build files
+cp -r ../.gh-pages/* .
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
@@ -43,10 +43,10 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../id_rsa_deploy.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
 
-# # Now that we're all set up, we can push.
+# # # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
