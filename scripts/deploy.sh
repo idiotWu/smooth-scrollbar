684c4b5822d8ca25f16d2a63da2b612df5f2b600
Dolphin_Wood <dolphin.w.e@gmail.com>
2016-05-19 21:04:31 +0900

    chore(travis): change deploy flow

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,16 +12,19 @@ fi
 
 # Save some useful information
 REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SSH_REPO=${REPO/\/\/github.com/\/\/$GITHUB_TOKEN@github.com}
 SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
 git clone $REPO out
 cd out
+
+echo "Checking out ${TARGET_BRANCH} branch"
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 
 # Clean out existing contents
+echo "Removing old contents"
 cd ..
 rm -rf ./out/**/* || exit 0
 cd out
@@ -31,6 +34,7 @@ git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # Copy build files
+echo "Copying new contents"
 cp -r ../.gh-pages/* .
 
 # Commit the "changes", i.e. the new version.
@@ -38,15 +42,5 @@ cp -r ../.gh-pages/* .
 git add .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../id_rsa_deploy.enc -out deploy_key -d
-chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
-
-# # # Now that we're all set up, we can push.
+# Now we can push.
 git push $SSH_REPO $TARGET_BRANCH
