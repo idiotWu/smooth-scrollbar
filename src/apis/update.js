63e565284ee4428ffc798dc8ab69816484af3107
Dolphin_Wood <dolphin.w.e@gmail.com>
2016-05-24 01:24:44 +0900

    fix(update): scrollbar#limit props stay above zero, resolve #24

--- src/apis/update.js
+++ src/apis/update.js
@@ -9,6 +9,103 @@ import { SmoothScrollbar } from '../smooth_scrollbar';
 export { SmoothScrollbar };
 
 /**
+ * @this-binding
+ *
+ * Update canvas size
+ */
+function updateCanvas() {
+    if (this.options.overscrollEffect !== 'glow') return;
+
+    const { targets, size } = this;
+    const { elem, context } = targets.canvas;
+    const DPR = window.devicePixelRatio || 1;
+
+    elem.width = size.container.width * DPR;
+    elem.height = size.container.height * DPR;
+
+    context.scale(DPR, DPR);
+};
+
+/**
+ * @this-binding
+ *
+ * Update scrollbar track and thumb
+ */
+function updateTrack() {
+    const {
+        size,
+        thumbSize,
+        targets: { xAxis, yAxis }
+    } = this;
+
+    // hide scrollbar if content size less than container
+    setStyle(xAxis.track, {
+        'display': size.content.width <= size.container.width ? 'none' : 'block'
+    });
+    setStyle(yAxis.track, {
+        'display': size.content.height <= size.container.height ? 'none' : 'block'
+    });
+
+    // use percentage value for thumb
+    setStyle(xAxis.thumb, {
+        'width': `${thumbSize.x}px`
+    });
+    setStyle(yAxis.thumb, {
+        'height': `${thumbSize.y}px`
+    });
+}
+
+/**
+ * @this-binding
+ *
+ * Re-calculate sizes:
+ *     1. offset limit
+ *     2. thumb sizes
+ */
+function update() {
+    const {
+        limit,
+        options
+    } = this;
+
+    this.__updateBounding();
+
+    const size = this.getSize();
+    const newLimit = {
+        x: Math.max(size.content.width - size.container.width, 0),
+        y: Math.max(size.content.height - size.container.height, 0)
+    };
+
+    this.__readonly('size', size);
+
+    if (newLimit.x === limit.x &&
+        newLimit.y === limit.y) {
+        return this::updateCanvas();
+    }
+
+    const thumbSize = {
+        // real thumb sizes
+        realX: size.container.width / size.content.width * size.container.width,
+        realY: size.container.height / size.content.height * size.container.height
+    };
+
+    // rendered thumb sizes
+    thumbSize.x = Math.max(thumbSize.realX, options.thumbMinSize);
+    thumbSize.y = Math.max(thumbSize.realY, options.thumbMinSize);
+
+    this.__readonly('limit', newLimit)
+        .__readonly('thumbSize', thumbSize);
+
+    // update appearance
+    this::updateTrack();
+    this::updateCanvas();
+
+    // re-positioning
+    this.setPosition();
+    this.__setThumbPosition();
+};
+
+/**
  * @method
  * @api
  * Update scrollbars appearance
@@ -16,81 +113,9 @@ export { SmoothScrollbar };
  * @param {Boolean} async: update asynchronous
  */
 SmoothScrollbar.prototype.update = function(async = true) {
-    // update canvas
-    let updateCanvas = () => {
-        const { targets, options, size } = this;
-        const { elem, context } = targets.canvas;
-
-        if (options.overscrollEffect === 'glow') {
-            const DPR = window.devicePixelRatio || 1;
-
-            elem.width = size.container.width * DPR;
-            elem.height = size.container.height * DPR;
-
-            context.scale(DPR, DPR);
-        }
-    };
-
-    let update = () => {
-        this.__updateBounding();
-
-        let size = this.getSize();
-
-        this.__readonly('size', size);
-
-        let newLimit = {
-            x: size.content.width - size.container.width,
-            y: size.content.height - size.container.height
-        };
-
-        if (this.limit &&
-            newLimit.x === this.limit.x &&
-            newLimit.y === this.limit.y) {
-            return updateCanvas();
-        }
-
-        const { targets, options } = this;
-
-        let thumbSize = {
-            // real thumb sizes
-            realX: size.container.width / size.content.width * size.container.width,
-            realY: size.container.height / size.content.height * size.container.height
-        };
-
-        // rendered thumb sizes
-        thumbSize.x = Math.max(thumbSize.realX, options.thumbMinSize);
-        thumbSize.y = Math.max(thumbSize.realY, options.thumbMinSize);
-
-        this.__readonly('limit', newLimit)
-            .__readonly('thumbSize', thumbSize);
-
-        const { xAxis, yAxis } = targets;
-
-        // hide scrollbar if content size less than container
-        setStyle(xAxis.track, {
-            'display': size.content.width <= size.container.width ? 'none' : 'block'
-        });
-        setStyle(yAxis.track, {
-            'display': size.content.height <= size.container.height ? 'none' : 'block'
-        });
-
-        // use percentage value for thumb
-        setStyle(xAxis.thumb, {
-            'width': `${thumbSize.x}px`
-        });
-        setStyle(yAxis.thumb, {
-            'height': `${thumbSize.y}px`
-        });
-
-        // re-positioning
-        this.setPosition();
-        this.__setThumbPosition();
-        updateCanvas();
-    };
-
     if (async) {
-        requestAnimationFrame(update);
+        requestAnimationFrame(this::update);
     } else {
-        update();
+        this::update();
     }
 };
