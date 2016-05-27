1f1c0765ea674d1f37c6f147a63b7dfb855a1a66
Dolphin_Wood <dolphin.w.e@gmail.com>
2016-05-27 14:24:51 +0900

    update(demo): prevent horizontal overflow

--- test/scripts/index.js
+++ test/scripts/index.js
@@ -5,3 +5,15 @@ import Scrollbar from '../../src/';
 window.Scrollbar = Scrollbar;
 Prism.highlightAll();
 document.getElementById('version').textContent = Scrollbar.version;
+
+if (window.innerWidth < 800) {
+    [...document.querySelectorAll('pre, table')].forEach((el) => {
+        const wrap = document.createElement('div');
+        wrap.className = 'wrap';
+
+        el.parentNode.insertBefore(wrap, el);
+        wrap.appendChild(el);
+
+        Scrollbar.init(wrap);
+    });
+}
