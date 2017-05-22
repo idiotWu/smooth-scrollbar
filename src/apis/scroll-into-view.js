bc6c788d187ebd3fafb3cdfb504efd0d41523112
Dolphin_Wood <dolphin.w.e@gmail.com>
2017-05-22 18:12:03 +0900

    chore: fix typo

--- src/apis/scroll-into-view.js
+++ src/apis/scroll-into-view.js
@@ -12,7 +12,7 @@ import { SmoothScrollbar } from '../smooth-scrollbar';
  *
  * @param  {Element} target                      target element
  * @param  {Boolean} options.alignToTop          whether to align to the top or the bottom edge of container
- * @param  {Boolean} options.onlyScrollIfNeeded  whether scroll container when target element is visible
+ * @param  {Boolean} options.onlyScrollIfNeeded  whether to scroll container when target element is visible
  * @param  {Number}  options.offsetTop           scrolling stop offset to top
  * @param  {Number}  options.offsetLeft          scrolling stop offset to left
  * @param  {Number}  options.offsetBottom        scrolling stop offset to bottom
