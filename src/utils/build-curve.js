f8ba42972e6b7f6e14ee4fe8bf71ec846115cbfd
Dolphin_Wood <dolphin.w.e@gmail.com>
2017-08-31 08:26:14 +0900

    fix(scrollTo): fix scrolling curves while duration=0 fixes #94

--- src/utils/build-curve.js
+++ src/utils/build-curve.js
@@ -13,9 +13,11 @@
  * @return {Array}: points
  */
 export const buildCurve = (distance, duration) => {
-    const res = [];
+    if (duration <= 0) {
+        return [distance];
+    }
 
-    if (duration <= 0) return res;
+    const res = [];
 
     const n = Math.round(duration / 1000 * 60) - 1;
     const d = distance ? Math.pow(1 / Math.abs(distance), 1 / n) : 0;
