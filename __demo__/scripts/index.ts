2357c99c34225e59919a2858cba30e04f118886d
Dolphin_Wood <dolphin.w.e@gmail.com>
2017-10-10 15:50:21 +0900

    update(demo): remove scrollbars on pre,td

--- __demo__/scripts/index.ts
+++ __demo__/scripts/index.ts
@@ -14,22 +14,3 @@ declare var __SCROLLBAR_VERSION__: string;
 Prism.highlightAll(false);
 
 (document.getElementById('version') as HTMLElement).textContent = __SCROLLBAR_VERSION__;
-
-if (window.innerWidth < 800) {
-  Array.from(document.querySelectorAll('pre, table')).forEach((el: HTMLElement) => {
-    const wrap = document.createElement('div');
-
-    if (el.parentNode) {
-      el.parentNode.insertBefore(wrap, el);
-    }
-
-    wrap.className = 'wrap';
-    wrap.appendChild(el);
-
-    Scrollbar.init(wrap, {
-      plugins: {
-        overscroll: false,
-      },
-    });
-  });
-}
