767ca2c2bf84cd0d3f0db65ebb1c66bbaaf149a6
Dolphin_Wood <dolphin.w.e@gmail.com>
2017-11-28 23:52:58 +0900

    fix(event/wheel): fix wheel event name in IE10 See #124

--- src/events/wheel.ts
+++ src/events/wheel.ts
@@ -4,21 +4,14 @@ import {
   eventScope,
 } from '../utils/';
 
-const DELTA_SCALE = {
-  STANDARD: 1,
-  OTHERS: -3,
-};
-
-const DELTA_MODE = [1.0, 28.0, 500.0];
-
-const getDeltaMode = (mode) => DELTA_MODE[mode] || DELTA_MODE[0];
-
 export function wheelHandler(scrollbar: I.Scrollbar) {
   const addEvent = eventScope(scrollbar);
 
   const target = scrollbar.options.wheelEventTarget || scrollbar.containerEl;
 
-  addEvent(target, 'onwheel' in window ? 'wheel' : 'mousewheel', (evt: WheelEvent) => {
+  const eventName = ('onwheel' in window || document.implementation.hasFeature('Events.wheel', '3.0')) ? 'wheel' : 'mousewheel';
+
+  addEvent(target, eventName, (evt: WheelEvent) => {
     const { x, y } = normalizeDelta(evt);
 
     scrollbar.addTransformableMomentum(x, y, evt, (willScroll) => {
@@ -29,9 +22,17 @@ export function wheelHandler(scrollbar: I.Scrollbar) {
   });
 }
 
-/**
- * Normalizing wheel delta
- */
+// Normalizing wheel delta
+
+const DELTA_SCALE = {
+  STANDARD: 1,
+  OTHERS: -3,
+};
+
+const DELTA_MODE = [1.0, 28.0, 500.0];
+
+const getDeltaMode = (mode) => DELTA_MODE[mode] || DELTA_MODE[0];
+
 function normalizeDelta(evt: WheelEvent) {
   if ('deltaX' in evt) {
     const mode = getDeltaMode(evt.deltaMode);
