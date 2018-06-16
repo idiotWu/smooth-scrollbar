4c9f0326944a9b2499cdfe9d553ef55c834e661c
Dolphin_Wood <dolphin.w.e@gmail.com>
2018-06-16 17:14:58 +0900

    fix(events): add passive event detection

--- src/utils/event-hub.ts
+++ src/utils/event-hub.ts
@@ -10,7 +10,32 @@ type EventConfig = {
   handler: EventHandler,
 };
 
+let eventListenerOptions: boolean | EventListenerOptions;
+
 const eventMap = new WeakMap<Scrollbar, EventConfig[]>();
+
+function getOptions(): typeof eventListenerOptions {
+  if (eventListenerOptions !== undefined) {
+    return eventListenerOptions;
+  }
+
+  let supportPassiveEvent = false;
+
+  try {
+    const noop = () => {};
+    const options = Object.defineProperty({}, 'passive', {
+      get() {
+        supportPassiveEvent = true;
+      },
+    });
+    window.addEventListener('testPassive', noop, options);
+    window.removeEventListener('testPassive', noop, options);
+  } catch (e) {}
+
+  eventListenerOptions = supportPassiveEvent ? { passive: false } as EventListenerOptions : false;
+
+  return eventListenerOptions;
+}
 
 export function eventScope(scrollbar: Scrollbar) {
   const configs = eventMap.get(scrollbar) || [];
@@ -34,7 +59,7 @@ export function eventScope(scrollbar: Scrollbar) {
     events.split(/\s+/g).forEach((eventName) => {
       configs.push({ elem, eventName, handler });
 
-      elem.addEventListener(eventName, handler);
+      elem.addEventListener(eventName, handler, getOptions());
     });
   };
 }
@@ -47,7 +72,7 @@ export function clearEventsOn(scrollbar: Scrollbar) {
   }
 
   configs.forEach(({ elem, eventName, handler }) => {
-    elem.removeEventListener(eventName, handler);
+    elem.removeEventListener(eventName, handler, getOptions());
   });
 
   eventMap.delete(scrollbar);
