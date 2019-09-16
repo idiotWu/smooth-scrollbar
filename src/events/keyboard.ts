93884fd0df085b5a5286896283bed29ec6e92577
Dolphin_Wood <dolphin.w.e@gmail.com>
2019-09-16 21:10:36 +0800

    chore(style): fix code style in events/keyboard

--- src/events/keyboard.ts
+++ src/events/keyboard.ts
@@ -98,12 +98,11 @@ function handleTabKey(scrollbar: I.Scrollbar) {
 }
 
 function isEditable(elem: any): boolean {
- if (
-    elem.tagName === "INPUT" ||
-    elem.tagName === "TEXTAREA" ||
-    elem.isContentEditable
-  ) {
+  if (elem.tagName === 'INPUT' ||
+      elem.tagName === 'TEXTAREA' ||
+      elem.isContentEditable) {
     return !elem.disabled;
   }
+
   return false;
 }
