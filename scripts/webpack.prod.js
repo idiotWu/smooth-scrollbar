b2af92b6b763bd759a7f2d4dedcfb7095dbd9234
HanJeaHwan <hanjeahwan@gmail.com>
2020-03-22 15:27:56 +0800

    fix(webpack): make UMD build available on both browsers and Node.js

--- scripts/webpack.prod.js
+++ scripts/webpack.prod.js
@@ -18,6 +18,7 @@ module.exports = merge(baseConfig, {
     library: 'Scrollbar',
     libraryTarget: 'umd',
     libraryExport: 'default',
+    globalObject: 'this'
   },
   plugins: [
     new UglifyJSPlugin(),
