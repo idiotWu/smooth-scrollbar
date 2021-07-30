a1e800f21e9b540ce0aa414501107f82b3f45693
Dolphin Wood <dolphin.w.e@gmail.com>
2021-07-30 17:32:42 +0900

    fix(geometry): add container's paddings to content's size

--- src/geometry/get-size.ts
+++ src/geometry/get-size.ts
@@ -6,6 +6,18 @@ export function getSize(scrollbar: I.Scrollbar): I.ScrollbarSize {
     contentEl,
   } = scrollbar;
 
+  const containerStyles = getComputedStyle(containerEl);
+  const paddings = [
+    'paddingTop',
+    'paddingBottom',
+    'paddingLeft',
+    'paddingRight',
+  ].map(prop => {
+    return containerStyles[prop] ? parseFloat(containerStyles[prop]) : 0;
+  });
+  const verticalPadding = paddings[0] + paddings[1];
+  const horizontalPadding = paddings[2] + paddings[3];
+
   return {
     container: {
       // requires `overflow: hidden`
@@ -13,9 +25,9 @@ export function getSize(scrollbar: I.Scrollbar): I.ScrollbarSize {
       height: containerEl.clientHeight,
     },
     content: {
-      // border width should be included
-      width: contentEl.offsetWidth - contentEl.clientWidth + contentEl.scrollWidth,
-      height: contentEl.offsetHeight - contentEl.clientHeight + contentEl.scrollHeight,
+      // border width and paddings should be included
+      width: contentEl.offsetWidth - contentEl.clientWidth + contentEl.scrollWidth + horizontalPadding,
+      height: contentEl.offsetHeight - contentEl.clientHeight + contentEl.scrollHeight + verticalPadding,
     },
   };
 }
