abd3acf8dbfb25a8f87662935c8ff953cde701d0
Sadegh Barati <sadeghbaratiwork@gmail.com>
2022-10-17 03:56:23 -0700

    refactor(utils): update

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,26 +1,23 @@
 
-export function debounce(fn: Function, wait?: number, leading?: boolean) {
-  let timer;
+export function debounce<T extends (...args: any[]) => void>(fn: T, wait?: number, leading?: boolean) {
+  let timer: number | NodeJS.Timeout;
   let lastCalledAt = -Infinity;
 
-  function handleLeading(thisArg, args) {
-    const now = Date.now();
-    const elapsed = now - lastCalledAt;
 
-    lastCalledAt = now;
-
-    if (elapsed >= wait!) {
-      fn.apply(thisArg, args);
-    }
-  }
-
-  return function debouncedFn(this: unknown, ...args) {
+  return function debouncedFn(this: unknown, ...args: Parameters<T>) {
     if (leading) {
-      handleLeading(this, args);
+      const now = Date.now();
+      const elapsed = now - lastCalledAt;
+
+      lastCalledAt = now;
+
+      if (elapsed >= wait!) {
+        fn.apply(this, args);
+      }
       return;
     }
 
-    clearTimeout(timer);
+    clearTimeout(timer as number);
 
     timer = setTimeout(() => {
       fn.apply(this, args);
