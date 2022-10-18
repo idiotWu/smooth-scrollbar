39e23f3ecb8a1060882408f68627805655a6286a
Sadegh Barati <sadeghbaratiwork@gmail.com>
2022-10-18 09:07:05 +0330

    refactor(utils): cleanup and improve types

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,19 +1,17 @@
-
-export function debounce<T extends (...args: any[]) => void>(fn: T, wait?: number, leading?: boolean) {
-  let timer: number | NodeJS.Timeout;
+export function debounce<T extends (...args: any[]) => void>(fn: T, wait: number = 0, leading?: boolean) {
+  let timer: number | ReturnType<typeof setTimeout>;
   let lastCalledAt = -Infinity;
-
 
   return function debouncedFn(this: unknown, ...args: Parameters<T>) {
     if (leading) {
       const now = Date.now();
       const elapsed = now - lastCalledAt;
-
       lastCalledAt = now;
 
-      if (elapsed >= wait!) {
+      if (elapsed >= wait) {
         fn.apply(this, args);
       }
+    } else {
       return;
     }
 
